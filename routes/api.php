e7c7aafe062b7fba12d5a78ec7cf32218ccb4531
Carlos <dizz01@gmail.com>
2025-03-29 18:25:23 +0100

    Ruta /ping para prueba de autodeploy

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/ping', function () {
-    return response()->json(['pong' => true]);
+    return response()->json(['pong' => "25"]);
 });
 
