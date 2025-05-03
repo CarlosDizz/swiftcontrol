75c8dcabd3675afa0bddb219e3b0203192df769b
Carlos <dizz01@gmail.com>
2025-05-03 12:19:35 +0200

    -CSS

--- resources/views/vendor/filament-panels/components/layout/base.blade.php
+++ resources/views/vendor/filament-panels/components/layout/base.blade.php
@@ -17,6 +17,7 @@
         <meta charset="utf-8" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="{{ asset('css/app/theme.css') }}">
 
         @if ($favicon = filament()->getFavicon())
             <link rel="icon" href="{{ $favicon }}" />
